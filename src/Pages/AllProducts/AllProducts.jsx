f62255aa0725c98cf242f2b933273e1010c40aec
Loki343 <kausikbhaumik42@gmail.com>
2023-06-08 00:02:01 +0530

    update

--- src/Pages/AllProducts/AllProducts.jsx
+++ src/Pages/AllProducts/AllProducts.jsx
@@ -53,7 +53,7 @@ function AllProducts() {
   );
   const [orderBy, SetOrderBy] = useState("");
   const [filterBy, setFilterBy] = useState("");
-// kkk
+// kk
   const sort = "price";
   const limit = 8;
 
